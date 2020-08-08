a404fb16d3c49ffc3cbdf05c287dda5aa8aac77a
Ishan Madan <ishan@mistlogic.com>
2020-08-08 01:18:43 -0700

    straws & spoon move independently, assembly values linked to straws

--- zippy-catapult/app.js
+++ zippy-catapult/app.js
@@ -21,14 +21,15 @@ var straw2;
 
 var spoonAngleMin = 0;
 var spoonAngle = Math.PI / 4;
+var spoonAngle = Math.PI * 0.46;
 var spoonAngleMax = Math.PI * 0.46;
 let spoonAngleFactor = 0.01;
 var spoonUpward = true;
 
-var strawYMin = -2;
-var strawY = -1.28175;
-var strawYMax = -0.5635;
-let strawYFactor = 0.01;
+var strawMin = 0;
+var strawMax = 4 - (0.03937008 + 0.5635 + 0.5635);
+var strawLength = strawMax;
+let strawFactor = 0.01;
 var strawUpward = true;
 
 let movement = true;
@@ -43,11 +44,11 @@ function init() {
     document.body.appendChild(container);
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    // camera.position.z = 10;
-    camera.position.x = -15;
-    camera.position.y = -1;
+    camera.position.z = 10;
+    /* camera.position.x = -15;
+    camera.position.y = 2;
     camera.position.z = -0.25;
-    camera.rotation.y = -Math.PI / 2;
+    camera.rotation.y = -Math.PI / 2; */
 
     // scene
 
@@ -70,13 +71,12 @@ function init() {
 
         }); */
 
-        assembly.position.y = -2;
         scene.add(assembly);
 
         {
             spoon.position.x = 0;
-            spoon.position.y = 0.56 * Math.sin(spoonAngle) - 2;
-            spoon.position.z = 0.56 * Math.cos(spoonAngle);
+            spoon.position.y = 0.5635 * Math.sin(spoonAngle) + 0.60287008;
+            spoon.position.z = 0.5635 * Math.cos(spoonAngle);
 
             spoon.rotation.x = -spoonAngle;
             spoon.rotation.y = -Math.PI / 2;
@@ -86,7 +86,7 @@ function init() {
 
         {
             straw1.position.x = 3.12;
-            straw1.position.y = strawY;
+            straw1.position.y = 0;
             straw1.position.z = 0;
 
             straw1.rotation.x = 0;
@@ -97,13 +97,13 @@ function init() {
         }
 
         {
-            straw2.position.x = -3.12;
-            straw2.position.y = strawY;
-            straw2.position.z = 0;
+            straw2.position.x = -straw1.position.x;
+            straw2.position.y = straw1.position.y;
+            straw2.position.z = straw1.position.z;
 
-            straw2.rotation.x = 0;
-            straw2.rotation.y = 0;
-            straw2.rotation.z = Math.PI / 2;
+            straw2.rotation.x = straw1.rotation.x;
+            straw2.rotation.y = straw1.rotation.y;
+            straw2.rotation.z = straw1.rotation.z;
 
             scene.add(straw2);
         }
@@ -211,59 +211,56 @@ function animate() {
 
 function render() {
 
-    /* camera.position.x += (mouseX * mouseFactor - camera.position.x) * speedFactor;
+    camera.position.x += (mouseX * mouseFactor - camera.position.x) * speedFactor;
     camera.position.y += (- mouseY * mouseFactor - camera.position.y) * speedFactor;
 
-    camera.lookAt(scene.position); */
+    camera.lookAt(scene.position);
 
     if (movement) {
-        if (strawY <= strawYMin) {
-            strawUpward = true;
-        } else if (strawY >= strawYMax) {
-            strawUpward = false;
+        {
+            if (strawLength <= strawMin) {
+                strawUpward = true;
+            } else if (strawLength >= strawMax) {
+                strawUpward = false;
+            }
+
+            if (strawUpward) {
+                strawLength += strawFactor
+            } else {
+                strawLength -= strawFactor
+            }
         }
 
-        if (strawUpward) {
-            strawY += strawYFactor
-        } else {
-            strawY -= strawYFactor
+        let assemblyAngle = Math.atan(strawLength / 4.871);
+        assembly.rotation.x = -assemblyAngle;
+        assembly.position.y = -4.871 * Math.sin(-assemblyAngle);
+        assembly.position.z = 4.871 * Math.cos(assemblyAngle) - 4.871;
+
+        straw1.rotation.x = -assemblyAngle;
+        straw1.position.z = strawLength * strawLength / 4.871 + 1.3 * assembly.position.z
+
+        straw2.rotation.x = straw1.rotation.x;
+        straw2.position.z = straw1.position.z;
+
+        {
+            if (spoonAngle <= spoonAngleMin) {
+                spoonUpward = true;
+            } else if (spoonAngle >= spoonAngleMax) {
+                spoonUpward = false;
+            }
+
+            if (spoonUpward) {
+                spoonAngle += spoonAngleFactor
+            } else {
+                spoonAngle -= spoonAngleFactor
+            }
+
+            spoon.position.y = 0.5635 * Math.sin(spoonAngle + assemblyAngle) + 0.60287008 + assembly.position.y;
+            spoon.position.z = 0.5635 * Math.cos(spoonAngle + assemblyAngle) + assembly.position.z;
+
+            spoon.rotation.x = -spoonAngle - assemblyAngle;
         }
     }
-
-    /* {
-        if (spoonAngle <= spoonAngleMin) {
-            spoonUpward = true;
-        } else if (spoonAngle >= spoonAngleMax) {
-            spoonUpward = false;
-        }
-
-        if (spoonUpward) {
-            spoonAngle += spoonAngleFactor
-        } else {
-            spoonAngle -= spoonAngleFactor
-        }
-
-        spoon.position.y = 0.56 * Math.sin(spoonAngle) - 2;
-        spoon.position.z = 0.56 * Math.cos(spoonAngle);
-
-        spoon.rotation.x = -spoonAngle;
-    } */
-
-    let strawLength = (0.5635 + strawY) * 2;
-
-    // let assemblyAngle = Math.atan(strawLength / 5.433);
-    let assemblyAngle = Math.asin(strawLength / 5.433);
-    assembly.rotation.x = assemblyAngle;
-    assembly.position.y = 5.433 * Math.sin(-assemblyAngle) - 2;
-    assembly.position.z = 5.433 * Math.cos(assemblyAngle) - 5.433;
-
-    straw1.rotation.x = assemblyAngle;
-    straw1.position.y = strawY;
-    straw1.position.z = assembly.position.z/*  + strawLength * strawLength / 5.433 */;
-
-    straw2.rotation.x = straw1.rotation.x;
-    straw2.position.y = straw1.position.y;
-    straw2.position.z = straw1.position.z;
 
     renderer.render(scene, camera);
 }
