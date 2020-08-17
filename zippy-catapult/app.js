326dfd1208ccf3c08b28a83648e13e33f1230ec3
Ishan Madan <ishan@mistlogic.com>
2020-08-16 22:20:27 -0700

    created a favicon, made zippycatapult interactive

--- zippy-catapult/app.js
+++ zippy-catapult/app.js
@@ -8,7 +8,7 @@ var camera, scene, renderer;
 
 var mouseX = 0, mouseY = 0;
 
-var mouseFactor = 0.02;
+var mouseFactor = 0.4;
 var speedFactor = 0.05;
 
 var windowHalfX = window.innerWidth / 2;
@@ -18,21 +18,52 @@ var assembly;
 var spoon;
 var straw1;
 var straw2;
+var ball;
 
-var spoonAngleMin = 0;
-var spoonAngle = Math.PI / 4;
-var spoonAngle = Math.PI * 0.46;
-var spoonAngleMax = Math.PI * 0.46;
+let spoonAngleMin = Math.PI * 0.04;
+let spoonAngleDefaultMax = Math.PI * 0.5;
+var spoonAngleMax = spoonAngleDefaultMax;
+var spoonAngle = spoonAngleMax / 2;
 let spoonAngleFactor = 0.01;
 var spoonUpward = true;
 
-var strawMin = 0;
-var strawMax = 4 - (0.03937008 + 0.5635 + 0.5635);
-var strawLength = strawMax;
+let strawMin = 0;
+let strawMax = 4 - (0.03937008 + 0.5635 + 0.5635);
+var strawLength = strawMax / 2;
+// let strawFactor = 0.01;
 let strawFactor = 0.01;
 var strawUpward = true;
 
-let movement = true;
+let movement = false;
+
+let fixedCamera = false;
+
+let timeFactor = 0.01;
+var t = 0;
+var v0x = 0;
+var v0y = 0;
+var vx = 0;
+var vy = 0;
+var ax = 0;
+var ay = 0;
+
+var x = 0;
+var y = 0;
+
+let targetPos = new THREE.Vector3(0, 0, 0);
+
+var material = new THREE.LineBasicMaterial({ color: 0xffffff });
+var linePoints1 = [];
+var line1;
+
+var linePoints2 = [];
+var line2;
+
+var mainPoints = [];
+var mainLine;
+
+var fontLoader = new THREE.FontLoader();
+var mesh;
 
 init();
 animate();
@@ -41,14 +72,20 @@ animate();
 function init() {
 
     container = document.createElement('div');
+    container.id = "container";
     document.body.appendChild(container);
 
+    var submitInputsButton = document.getElementById("submit");
+    submitInputsButton.onclick = submitInputs;
+
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.z = 10;
-    /* camera.position.x = -15;
+    camera.position.z = 20;
     camera.position.y = 2;
-    camera.position.z = -0.25;
-    camera.rotation.y = -Math.PI / 2; */
+    if (fixedCamera) {
+        camera.position.x = -15;
+        camera.position.y = 20;
+        camera.position.z = -0.25;
+    }
 
     // scene
 
@@ -74,23 +111,20 @@ function init() {
         scene.add(assembly);
 
         {
-            spoon.position.x = 0;
-            spoon.position.y = 0.5635 * Math.sin(spoonAngle) + 0.60287008;
-            spoon.position.z = 0.5635 * Math.cos(spoonAngle);
+            spoon.position.x = 0.07;
+            // spoon.position.y = 0.5635 * Math.sin(spoonAngle) + 0.60287008;
+            // spoon.position.z = 0.5635 * Math.cos(spoonAngle);
 
-            spoon.rotation.x = -spoonAngle;
+            // spoon.rotation.x = -spoonAngle;
             spoon.rotation.y = -Math.PI / 2;
             spoon.rotation.z = -Math.PI / 2;
+
             scene.add(spoon);
         }
 
         {
             straw1.position.x = 3.12;
-            straw1.position.y = 0;
-            straw1.position.z = 0;
 
-            straw1.rotation.x = 0;
-            straw1.rotation.y = 0;
             straw1.rotation.z = Math.PI / 2;
 
             scene.add(straw1);
@@ -98,16 +132,62 @@ function init() {
 
         {
             straw2.position.x = -straw1.position.x;
-            straw2.position.y = straw1.position.y;
-            straw2.position.z = straw1.position.z;
 
-            straw2.rotation.x = straw1.rotation.x;
-            straw2.rotation.y = straw1.rotation.y;
             straw2.rotation.z = straw1.rotation.z;
 
             scene.add(straw2);
         }
+
+        {
+            // ball.position.x = -0.07;
+            ball.position.y = spoon.position.y;
+            ball.position.z = spoon.position.z;
+
+            scene.add(ball);
+        }
     }
+
+    linePoints1.push(new THREE.Vector3(-1, 0, 0));
+    linePoints1.push(new THREE.Vector3(1, 0, 0));
+    var geometry1 = new THREE.BufferGeometry().setFromPoints(linePoints1);
+    line1 = new THREE.Line(geometry1, material);
+    scene.add(line1);
+
+    linePoints2.push(new THREE.Vector3(-1, 0, 0));
+    linePoints2.push(new THREE.Vector3(1, 0, 0));
+    var geometry2 = new THREE.BufferGeometry().setFromPoints(linePoints2);
+    line2 = new THREE.Line(geometry2, material);
+    scene.add(line2);
+
+    mainPoints.push(new THREE.Vector3(0, 0, 0));
+    mainPoints.push(new THREE.Vector3(0, 0, 0));
+    var mainGeometry = new THREE.BufferGeometry().setFromPoints(mainPoints);
+    mainLine = new THREE.Line(mainGeometry, material);
+    scene.add(mainLine);
+
+    fontLoader.load('https://unpkg.com/three/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+        var text = "Distance: " + (mainPoints[1].z - mainPoints[0].z);
+
+        var geometry = new THREE.TextGeometry(text, {
+            font: font,
+            size: 1,
+            height: 0.1,
+            curveSegments: 12,
+            bevelEnabled: false,
+            bevelThickness: 1,
+            bevelSize: 1,
+            bevelOffset: 0,
+            bevelSegments: 5
+        });
+
+        mesh = new THREE.Mesh(geometry, material);
+
+        mesh.rotation.x = Math.PI * 1.5;
+        mesh.position.x = -5.75;
+        mesh.visible = false;
+
+        scene.add(mesh);
+    });
 
     var manager = new THREE.LoadingManager(loadModel);
 
@@ -164,6 +244,12 @@ function init() {
             straw2 = obj;
 
         }, onProgress, onError);
+
+        loader.load('ball.obj', function (obj) {
+
+            ball = obj;
+
+        }, onProgress, onError);
     }
 
     //
@@ -211,10 +297,14 @@ function animate() {
 
 function render() {
 
-    camera.position.x += (mouseX * mouseFactor - camera.position.x) * speedFactor;
-    camera.position.y += (- mouseY * mouseFactor - camera.position.y) * speedFactor;
+    if (!fixedCamera) {
+        camera.position.x += (mouseX * mouseFactor - camera.position.x) * speedFactor;
+        camera.position.y += (- mouseY * mouseFactor - camera.position.y) * speedFactor;
+    }
 
-    camera.lookAt(scene.position);
+    targetPos.z = (scene.position.z + ball.position.z) / 2;
+
+    camera.lookAt(targetPos);
 
     if (movement) {
         {
@@ -231,17 +321,6 @@ function render() {
             }
         }
 
-        let assemblyAngle = Math.atan(strawLength / 4.871);
-        assembly.rotation.x = -assemblyAngle;
-        assembly.position.y = -4.871 * Math.sin(-assemblyAngle);
-        assembly.position.z = 4.871 * Math.cos(assemblyAngle) - 4.871;
-
-        straw1.rotation.x = -assemblyAngle;
-        straw1.position.z = strawLength * strawLength / 4.871 + 1.3 * assembly.position.z
-
-        straw2.rotation.x = straw1.rotation.x;
-        straw2.position.z = straw1.position.z;
-
         {
             if (spoonAngle <= spoonAngleMin) {
                 spoonUpward = true;
@@ -254,13 +333,136 @@ function render() {
             } else {
                 spoonAngle -= spoonAngleFactor
             }
+        }
+    } else if (ball.position.y > 0) {
+        if (mesh != undefined) {
+            mesh.visible = false;
+        }
+        vx = v0x + ax * t;
+        vy = v0y + ay * t;
 
-            spoon.position.y = 0.5635 * Math.sin(spoonAngle + assemblyAngle) + 0.60287008 + assembly.position.y;
-            spoon.position.z = 0.5635 * Math.cos(spoonAngle + assemblyAngle) + assembly.position.z;
+        ball.position.z = x + t * 0.5 * (vx + v0x);
+        ball.position.y = y + t * 0.5 * (vy + v0y);
 
-            spoon.rotation.x = -spoonAngle - assemblyAngle;
+        t += timeFactor;
+        // console.log(vx + " " + vy + " " + v0x + " " + v0y + " " + ball.position.z + " " + ball.position.y + " " + t);
+
+        if (ball.position.z <= 0) {
+            line1.visible = false;
+            line2.visible = false;
+            mainLine.visible = false;
+        } else {
+            line1.visible = true;
+            line2.visible = true;
+            mainLine.visible = true;
+
+            linePoints2[0].z = ball.position.z;
+            linePoints2[1].z = ball.position.z;
+            mainPoints[1].z = ball.position.z;
+
+            line2.geometry.setFromPoints(linePoints2);
+            mainLine.geometry.setFromPoints(mainPoints);
+        }
+    } else if (ball.position.z > 1 && !mesh.visible) {
+        ball.position.y = 0;
+        fontLoader.load('https://unpkg.com/three/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+            var text = "Distance: " + (Math.round((mainPoints[1].z - mainPoints[0].z) * 100) / 100);
+
+            var geometry = new THREE.TextGeometry(text, {
+                font: font,
+                size: 1,
+                height: 0.1,
+                curveSegments: 12,
+                bevelEnabled: false,
+                bevelThickness: 1,
+                bevelSize: 1,
+                bevelOffset: 0,
+                bevelSegments: 5
+            });
+
+            mesh.geometry = geometry;
+
+            mesh.rotation.x = Math.PI * 1.5;
+            mesh.position.x = -6;
+            mesh.position.z = targetPos.z;
+            mesh.visible = true;
+        });
+    }
+
+    setPositions();
+    renderer.render(scene, camera);
+}
+
+function setPositions() {
+    let assemblyAngle = Math.atan(strawLength / 4.871);
+    assembly.rotation.x = -assemblyAngle;
+    assembly.position.y = -4.871 * Math.sin(-assemblyAngle);
+    assembly.position.z = 4.871 * Math.cos(assemblyAngle) - 4.871;
+
+    spoonAngleMax = spoonAngleDefaultMax + assemblyAngle;
+
+    straw1.rotation.x = -assemblyAngle;
+    straw1.position.z = strawLength * strawLength / 4.871 + 1.3 * assembly.position.z
+
+    straw2.rotation.x = straw1.rotation.x;
+    straw2.position.z = straw1.position.z;
+
+    spoon.position.y = 0.5635 * Math.sin(Math.PI * 0.5 - spoonAngle + assemblyAngle) + 0.60287008 + assembly.position.y;
+    spoon.position.z = 0.5635 * Math.cos(Math.PI * 0.5 - spoonAngle + assemblyAngle) + assembly.position.z;
+
+    spoon.rotation.x = -(Math.PI * 0.5 - spoonAngle) - assemblyAngle;
+
+    if (ball.position.y == 0 && ball.position.z == 0 && !mesh.visible) {
+        ball.position.y = spoon.position.y + 5 * Math.sin(spoonAngle - assemblyAngle - Math.PI * 0.07);
+        ball.position.z = spoon.position.z - 5 * Math.cos(spoonAngle - assemblyAngle - Math.PI * 0.07);
+    }
+}
+
+function submitInputs() {
+    var strawLengthInput = document.getElementById("strawLength");
+    var spoonAngleInput = document.getElementById("spoonAngle");
+    var velocityInput = document.getElementById("velocity");
+    var gravityInput = document.getElementById("gravity");
+
+    strawLength = clip(strawLengthInput.value, strawMin, strawMax);
+    strawLengthInput.value = Math.round(strawLength * 100) / 100;
+
+    spoonAngle = clip(spoonAngleInput.value * Math.PI / 180, spoonAngleMin, spoonAngleMax);
+    spoonAngleInput.value = Math.round(spoonAngle / Math.PI * 180 * 100) / 100;
+
+    ball.position.y = 0;
+    ball.position.z = 0;
+    mesh.visible = false;
+    setPositions();
+
+    var angle = Math.PI - (spoon.rotation.x - Math.PI * 0.2 + Math.PI * 0.16);
+
+    while (angle > Math.PI || angle < 0) {
+        if (angle > Math.PI) {
+            angle -= Math.PI;
+        } else if (angle < 0) {
+            angle += Math.PI;
         }
     }
 
-    renderer.render(scene, camera);
+    x = ball.position.z;
+    y = ball.position.y;
+
+    t = 0;
+    v0x = velocityInput.value * Math.cos(angle);
+    v0y = velocityInput.value * Math.sin(angle);
+    vx = v0x;
+    vy = v0y;
+    ax = 0;
+    ay = -1 * Number(gravityInput.value);
+}
+
+function clip(input, limit1, limit2) {
+    if (input < limit1) {
+        return limit1;
+    } else if (input > limit2) {
+        return limit2;
+    } else {
+        return input;
+    }
 }
