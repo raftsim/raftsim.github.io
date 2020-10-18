b493f8ea5ee675196b6ca04bee7230f7096a96d5
Ishan Madan <ishan@mistlogic.com>
2020-10-18 03:13:26 -0700

    created/setup rollback can project & uploaded cad

---
+++ rollback-can/app.js
@@ -0,0 +1,139 @@
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+
+import { STLLoader } from 'https://unpkg.com/three/examples/jsm/loaders/STLLoader.js';
+
+import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
+
+var container;
+
+var camera, scene, renderer, controls;
+
+var assembly;
+
+let targetPos = new THREE.Vector3(0, 0, 0);
+
+var input1, input2;
+
+let input1Min = 0;
+let input1Max = 99;
+
+let input2Min = 100;
+let input2Max = 199;
+
+let input1Input = document.getElementById("input1");
+let input2Input = document.getElementById("input2");
+
+init();
+animate();
+
+function init() {
+
+    container = document.createElement('div');
+    container.id = "container";
+    document.body.appendChild(container);
+    
+    var submitInputsButton = document.getElementById("submit");
+    submitInputsButton.onclick = submitInputs;
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.x = 200;
+    camera.position.y = 200;
+    camera.position.z = 200;
+
+    // scene
+
+    scene = new THREE.Scene();
+
+    var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+    scene.add(ambientLight);
+
+    var pointLight = new THREE.PointLight(0xffffff, 0.8);
+    camera.add(pointLight);
+    scene.add(camera);
+
+    var loader = new STLLoader();
+
+    var material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+
+    loader.load('objects/assembly.stl', function (geometry) {
+
+        assembly = new THREE.Mesh(geometry, material);
+
+        scene.add(assembly);
+
+    });
+
+    //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    // orbit controls
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.update();
+    controls.enablePan = false;
+    controls.enableDamping = true;
+
+    //
+
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+//
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
+
+}
+
+function render() {
+
+    // TODO: Change camera target here
+
+    controls.target.set(targetPos.x, targetPos.y, targetPos.z);
+
+    // TODO: change object positions, rotations, states, etc here
+
+    renderer.render(scene, camera);
+}
+
+function clip(input, limit1, limit2) {
+    if (input < limit1) {
+        return limit1;
+    } else if (input > limit2) {
+        return limit2;
+    } else {
+        return input;
+    }
+}
+
+function submitInputs() {
+    document.getElementById("output-text").style.visibility = "hidden";
+
+    input1 = clip(input1Input.value, input1Min, input1Max);
+    input2 = clip(input2Input.value, input2Min, input2Max);
+
+    sendValues();
+}
+
+function sendValues() {
+    input1Input.value = Math.round(input1 * 100) / 100;
+    input2Input.value = Math.round(input2 * 100) / 100;
+}
