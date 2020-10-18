2f5b0084c0433235e855e5003a0149eb7c2f5bb4
Christopher Park <parkchris252@gmail.com>
2020-10-17 19:33:10 -0700

    Created Hovercraft project -- improted balloon.obj

---
+++ hovercraft/app.js
@@ -0,0 +1,146 @@
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+
+import { OBJLoader } from 'https://unpkg.com/three/examples/jsm/loaders/OBJLoader.js';
+
+import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
+
+var container;
+
+var camera, scene, renderer, controls;
+
+var balloon, base;
+
+let targetPos = new THREE.Vector3(0, 0, 0);
+
+init();
+animate();
+
+function init() {
+
+    container = document.createElement('div');
+    container.id = "container";
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.z = 20;
+    camera.position.y = 2;
+    
+    // scene
+
+    scene = new THREE.Scene();
+
+    var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+    scene.add(ambientLight);
+
+    var pointLight = new THREE.PointLight(0xffffff, 0.8);
+    camera.add(pointLight);
+    scene.add(camera);
+
+    // manager
+
+    function loadModel() {
+
+        scene.add(balloon);
+        // scene.add(base);
+console.log("hello")
+    }
+
+    var manager = new THREE.LoadingManager(loadModel);
+
+    manager.onProgress = function (item, loaded, total) {
+
+        console.log(item, loaded, total);
+
+    };
+
+    // model
+
+    function onProgress(xhr) {
+
+        if (xhr.lengthComputable) {
+
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log('model ' + Math.round(percentComplete, 2) + '% downloaded');
+
+        }
+
+    }
+
+    function onError() { }
+
+    {
+        var loader = new OBJLoader(manager);
+
+        loader.load('objects/balloon.obj', function (obj) { 
+          
+            balloon = obj;
+
+        }, onProgress, onError);
+        
+        /* loader.load('objects/base.obj', function (obj) { 
+          
+            base = obj;
+
+        }, onProgress, onError); */
+    }
+
+    //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    // orbit controls
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.update();
+    controls.enablePan = false;
+    controls.enableDamping = true;
+
+    //
+
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+//
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
+
+}
+
+function render() {
+    
+    // TODO: Change camera target here
+    
+    controls.target.set(targetPos.x, targetPos.y, targetPos.z);
+
+    // TODO: change object positions, rotations, states, etc here
+
+    renderer.render(scene, camera);
+}
+
+function clip(input, limit1, limit2) {
+    if (input < limit1) {
+        return limit1;
+    } else if (input > limit2) {
+        return limit2;
+    } else {
+        return input;
+    }
+}
