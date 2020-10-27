4fb4716b5518829092e25ec1a6fce46a2e9f8ad9
Kedaar Chakankar <kedaarchakankar@gmail.com>
2020-10-26 22:55:16 -0700

    I built the bridge.

--- leonardos-bridge/app.js
+++ leonardos-bridge/app.js
@@ -8,7 +8,7 @@ var container;
 
 var camera, scene, renderer, controls;
 
-var templateStick, sticks;
+var templateStick, sticks, group;
 
 let targetPos = new THREE.Vector3(0, 0, 0);
 
@@ -41,10 +41,11 @@ function init() {
 
     function loadModel() {
 
-        scene.add(templateStick);
+        //scene.add(templateStick);
         sticks = [];
         createSticks();
-        scene.add(sticks[1]);
+        //can we add those to the group since those were defaults
+        //scene.add(sticks[1]);
 
     }
 
@@ -111,27 +112,125 @@ function onWindowResize() {
 
 }
 
-//
+// is there a way to change the point of origion of the camera
 function createSticks() {
-
-    for (var i = 0; i < sticks.length; i++) {
+scene.remove (group);
+    /* for (var i = 0; i < sticks.length; i++) {
         scene.remove(sticks[i]);
-    }
-
+    } */
+    group = new THREE.Group();
     sticks = [];
 
-    for (var i = 0; i < 15; i++) {
+    for (var i = 0; i < 18; i++) {
         var newStick = templateStick.clone(true);
-        newStick.position.x = 50 * (i + 1);
+        newStick.position.x = 230 * (i + 1);
 
         sticks.push(newStick);
-        console.log(sticks[i].position.x);
-        scene.add(sticks[i]);
+        //console.log(sticks[i].position.x);
+        group.add(sticks[i]);
     }
-    console.log(sticks.length);
-    console.log(""+camera.position.x + camera.position.y + camera.position.z)
-}
 
+    sticks[14].position.x = 0;
+
+    sticks[0].position.x = -115;
+    sticks[0].position.z += -6.35;
+    sticks[0].rotation.y -= 0.07;
+
+    sticks[1].rotation.z += 1.5707963268;
+    sticks[1].position.x = 0;
+    sticks[1].position.y += 101.5;
+    sticks[1].position.z += -3.175;
+
+    sticks[2].rotation.z += 1.5707963268;
+    sticks[2].position.x = 0;
+    sticks[2].position.y -= 101.5;
+    sticks[2].position.z += -3.175;
+
+    sticks[3].position.x = 115;
+    sticks[3].position.z += -6.35;
+    sticks[3].rotation.y += 0.07;
+
+    sticks[4].rotation.z += 1.5707963268;
+    sticks[4].position.x = 115;
+    sticks[4].position.y += 74.5;
+    sticks[4].position.z -= 12;
+    sticks[4].rotation.y += 0.145;
+
+    sticks[5].rotation.z += 1.5707963268;
+    sticks[5].position.x = 115;
+    sticks[5].position.y -= 74.5;
+    sticks[5].position.z -= 12;
+    sticks[5].rotation.y += 0.145;
+
+    sticks[6].rotation.z += 1.5707963268;
+    sticks[6].position.x = -115;
+    sticks[6].position.y -= 74.5;
+    sticks[6].position.z -= 12;
+    sticks[6].rotation.y += -0.145;
+
+    sticks[7].rotation.z += 1.5707963268;
+    sticks[7].position.x = -115;
+    sticks[7].position.y += 74.5;
+    sticks[7].position.z -= 12;
+    sticks[7].rotation.y += -0.145;
+
+    sticks[8].position.x = -230;
+    sticks[8].position.z += -6.35;
+    sticks[8].position.z -= 26;
+    sticks[8].rotation.y += -0.145;
+
+    sticks[9].rotation.z += 1.5707963268;
+    sticks[9].position.x = -230;
+    sticks[9].position.y += 101.5;
+    sticks[9].position.z += -38.5;
+    sticks[9].rotation.y += -0.34;
+
+    sticks[10].rotation.z += 1.5707963268;
+    sticks[10].position.x = -230;
+    sticks[10].position.y -= 101.5;
+    sticks[10].position.z += -38.5;
+    sticks[10].rotation.y += -0.34;
+
+    sticks[11].position.x = -345;
+    sticks[11].position.z += -6.35;
+    sticks[11].position.z -= 77;
+    sticks[11].rotation.y += -0.4;
+
+    sticks[12].rotation.z += 1.5707963268;
+    sticks[12].position.x = -331.5;
+    sticks[12].position.y -= 74.5;
+    sticks[12].position.z -= 81;
+    sticks[12].rotation.y += -0.515;
+
+    sticks[13].rotation.z += 1.5707963268;
+    sticks[13].position.x = -331.5;
+    sticks[13].position.y += 74.5;
+    sticks[13].position.z -= 81;
+    sticks[13].rotation.y += -0.515;
+
+    sticks[15].position.x = 230;
+    sticks[15].position.z += -6.35;
+    sticks[15].position.z -= 26;
+    sticks[15].rotation.y += 0.145;
+    group.rotation.x -= 1.5707963268;
+    scene.add(group);
+
+    sticks[16].rotation.z += 1.5707963268;
+    sticks[16].position.x = 230;
+    sticks[16].position.y += 101.5;
+    sticks[16].position.z += -38.5;
+    sticks[16].rotation.y += 0.34;
+
+    sticks[17].rotation.z += 1.5707963268;
+    sticks[17].position.x = 230;
+    sticks[17].position.y -= 101.5;
+    sticks[17].position.z += -38.5;
+    sticks[17].rotation.y += 0.34;
+}
+// 0.25 kg - decreases by abt 30%
+// 0.5kg - decreases by abt 50%
+// 0.75kg - decreases by abt 70%
+//1kg - brings the whole thing down
 function animate() {
 
     requestAnimationFrame(animate);
@@ -142,8 +241,9 @@ function animate() {
 
 function render() {
     
-    // TODO: Change camera target here
-    
+    // thank u sir
+    targetPos.x = -57.5;
+    targetPos.z = -6.35;
     controls.target.set(targetPos.x, targetPos.y, targetPos.z);
 
     // TODO: change object positions, rotations, states, etc here
