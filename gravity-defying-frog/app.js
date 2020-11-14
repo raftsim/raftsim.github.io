f01dae559608552b1d94303e3bc457a8c3e7e4d4
Vishnu Velayuthan <100023569@mvla.net>
2020-11-13 22:41:44 -0800

    just in case, work in progress

--- gravity-defying-frog/app.js
+++ gravity-defying-frog/app.js
@@ -10,27 +10,27 @@ var camera, scene, renderer, controls;
 
 var base, frog;
 
-var rightHand, leftHand, rightLeg, leftLeg;
-
-var xToMove, yToMove, zToMove;
+var rightHand, leftHand, rightFoot, leftFoot;
 
 var xMove, yMove, zMove;
 
-// let tRate = 0.01;
-// let t = 0; 
+let move = new THREE.Euler3(0.68 - Math.PI/2, 0, 0);
 
 let tRate = 100;
 
-var xRate, yRate, zRate;
+var t = 0;
+
+var xRate = 0;
+var yRate = 0;
+var zRate = 0;
 
 let targetPos = new THREE.Vector3(0, 0, 0);
 
 let tfRightHand = document.getElementById("rightHand");
 let tfLeftHand = document.getElementById("leftHand");
-let tfRightLeg = document.getElementById("rightLeg");
-let tfLeftLeg = document.getElementById("leftLeg");
+let tfRightFoot = document.getElementById("rightFoot");
+let tfLeftFoot = document.getElementById("leftFoot");
 
-let btSubmit = document.getElementById("submit");
 
 init();
 animate();
@@ -41,13 +41,13 @@ function init() {
     container.id = "container";
     document.body.appendChild(container);
 
+    let btSubmit = document.getElementById("submit");
     btSubmit.onclick = submitInputs;
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
     camera.position.z = 200;
     camera.position.y = 200;
     camera.position.x = -200;
-
 
     // scene
 
@@ -78,9 +78,20 @@ function init() {
 
     loader.load('objects/frog.stl', function (geometry) {
 
+        console.log('big sur is cool');
+
         frog = new THREE.Mesh(geometry, material);
         resetFrog();
+        frog.rotation.z = 0;
+        frog.rotation.y = 0;
+        frog.rotation.x = 0.68 - Math.PI / 2;
+
+        zMove = 0;
+        yMove = 0;
+        xMove = 0.68 - Math.PI / 2;
         scene.add(frog);
+
+        console.log('catalina sucks');
 
     });
 
@@ -124,94 +135,46 @@ function animate() {
 
 }
 
+let threshold = 0.01;
+
 function render() {
 
     // TODO: Change camera target here
 
     controls.target.set(targetPos.x, targetPos.y, targetPos.z);
 
-    // TODO: change object positions, rotations, states, etc here
+    if (frog != undefined && t < tRate) {
+        console.log(frog.rotation);
+        frog.rotation.x += xRate;
+        frog.rotation.y += yRate;
+        frog.rotation.z += zRate;
+        t++;
+    }
 
     renderer.render(scene, camera);
 }
 
 function resetFrog() {
-    frog.rotation.z = 0;
-    frog.rotation.y = 0;
-    frog.rotation.x = 0.68 - Math.PI / 2;
+    // frog.rotation.z = 0;
+    // frog.rotation.y = 0;
+    // frog.rotation.x = 0.68 - Math.PI / 2;
 
-    zToMove = 0;
-    yToMove = 0;
-    xToMove = 0.68 - Math.PI / 2;
-
-    zMove = 0;
-    yMove = 0;
     xMove = 0.68 - Math.PI / 2;
-}
+    yMove = 0;
+    zMove = 0;
 
-//Add input number thing
-function onLeftHand() {
-    yMove += 0.3;
-    zMove -= 0.1;
-    xMove -= 0.113;
-}
-
-//Add input number thing
-function onRightHand() {
-    yMove -= 0.3;
-    zMove += 0.1;
-    xMove -= 0.113;
-}
-
-function onLeftLeg() {
-    yMove += 0.3;
-    zMove += 0.3;
-    xMove -= 0.68;
-}
-
-function onRightLeg() {
-    yMove += 0.3;
-    zMove += 0.3;
-    xMove += 0.68;
+    t = 0;
 }
 
 function paperclips(lh, rh, ll, rl) {
-    yMove += 0.3 * (lh - rh + ll + rl);
-    zMove += 0.1 * (lh - rh) + 0.3 * (rl - ll);
-    xMove += 0.113 * (-rh - lh) + 0.68 * (rl - ll);
+    let xLim = 1;
+    let yLim = 0.69;
+    let zLim = 0.95;
+
+    xMove = clip(( /* xMove + */ 0.68 - Math.PI / 2 + 0.113 * (-rh - lh) + 0.68 * (rl - ll)), -xLim, xLim);
+    yMove = clip(( /* yMove + */ 0.3 * (rh - lh + ll + rl)), -yLim, yLim);
+    zMove = clip(( /* zMove + */ 0.1 * (lh - rh) + 0.3 * (rl - ll)), -zLim, zLim);
 }
-
-function clipX(xRotation) {
-    if (xRotation < 0.69 && xRotation > -0.69) {
-        return xRotation;
-    }
-    if (xRotation < 0) {
-        return -0.69;
-    }
-    return 0.69;
-}
-
-function clipY(yRotation) {
-    if (yRotation < 1 && yRotation > -1) {
-        return yRotation;
-    }
-    if (yRotation < 0) {
-        return -1;
-    }
-    return 1;
-}
-
-function clipZ(zRotation) {
-    if (zRotation < 0.95 && zRotation > -0.95) {
-        return zRotation;
-    }
-    if (zRotation < 0) {
-        return -0.95;
-    }
-    return 0.95;
-}
-
-
 
 
 
@@ -232,18 +195,21 @@ function submitInputs() {
 
     rightHand = Math.round(tfRightHand.value);
     leftHand = Math.round(tfLeftHand.value);
-    rightLeg = Math.round(tfRightLeg.value);
-    leftLeg = Math.round(tfLeftLeg.value);
+    rightFoot = Math.round(tfRightFoot.value);
+    leftFoot = Math.round(tfLeftFoot.value);
 
-    paperclips(leftHand, rightHand, leftLeg, rightLeg);
+    paperclips(leftHand, rightHand, leftFoot, rightFoot);
 
-    xRate = xMove/tRate;
-    yRate = yMove/tRate;
-    zRate = zMove/tRate; 
+    xRate = xMove / tRate;
+    yRate = yMove / tRate;
+    zRate = zMove / tRate;
 
     sendValues();
 }
 
 function sendValues() {
     tfRightHand.value = rightHand;
+    tfLeftHand.value = leftHand;
+    tfRightFoot.value = rightFoot;
+    tfLeftFoot.value = leftFoot;
 }
