cfbc4801c4fee508faafd75af750ae54766c853d
Sainirnay Mantrala <sainirnay426@gmail.com>
2021-01-18 21:40:14 -0800

    working disk and slow down

--- black-and-white/app.js
+++ black-and-white/app.js
@@ -8,7 +8,21 @@ var container;
 
 var camera, scene, renderer, controls;
 
-var disk, knob, marble;
+var cutout, assembly, ttop, btop;
+
+var angle = 0;
+var input1 = 0;
+var input2;
+let input1Min = 0;
+let input1Max = 3;
+
+let input2Min = 100;
+let input2Max = 199;
+
+let input1Input = document.getElementById("input1");
+let input2Input = document.getElementById("input2");
+
+var rot = 0;
 
 let targetPos = new THREE.Vector3(0, 0, 0);
 
@@ -21,9 +35,14 @@ function init() {
     container.id = "container";
     document.body.appendChild(container);
 
+    var submitInputsButton = document.getElementById("submit");
+    submitInputsButton.onclick = submitInputs;
+
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.z = 20;
-    camera.position.y = 2;
+    camera.position.z = -40;
+    camera.position.y = -123;
+    //
+    
 
     // scene
 
@@ -36,14 +55,12 @@ function init() {
     camera.add(pointLight);
     scene.add(camera);
 
-    scene.background = new THREE.Color(0x7F4BC3);
-
     var loader = new STLLoader();
 
     var material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 
 
-    loader.load('objects/black-and-white-cd.stl', function (geometry) {
+    /* loader.load('objects/black-and-white-cd.stl', function (geometry) {
 
         disk = new THREE.Mesh(geometry, material);
 
@@ -64,6 +81,22 @@ function init() {
         marble = new THREE.Mesh(geometry, material);
 
         scene.add(marble);
+
+    }); */
+    loader.load('objects/assembly.stl', function (geometry) {
+
+        assembly = new THREE.Mesh(geometry, material);
+        assembly.rotation.x = -Math.PI
+        scene.add(assembly);
+
+
+    });
+    loader.load('objects/cutout.stl', function (geometry) {
+
+        cutout = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0x000000 }));
+        //scene.add(cutout);
+        cutout.rotation.x = -Math.PI
+        cutout.position.z -= 0.67;
 
     });
 
@@ -101,18 +134,24 @@ function init() {
         ); */
 
     var img = new THREE.MeshBasicMaterial({ //CHANGED to MeshBasicMaterial
-        map: THREE.ImageUtils.loadTexture('objects/benham-disk-pattern.jpg')
+        map: THREE.ImageUtils.loadTexture('objects/benham-disk-pattern.png')
     });
     img.map.needsUpdate = true; //ADDED
 
-    var plane = new THREE.Mesh(new THREE.PlaneGeometry(30, 30), img);
-    plane.overdraw = true;
-    scene.add(plane);
+    ttop = new THREE.Mesh(new THREE.PlaneGeometry(120, 120), img);
+    ttop.overdraw = true;
+    ttop.position.z -= 7.8;
+    scene.add(ttop);
 
+    btop = new THREE.Mesh(new THREE.PlaneGeometry(120, 120), img);
+    btop.overdraw = true;
+    btop.position.z -= 7.9;
+    btop.rotation.y = Math.PI;
+    btop.scale.x = -1;
+    scene.add(btop);
 
-
-
-
+    btop.rotation.x = -Math.PI
+    ttop.rotation.x = -Math.PI
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -126,6 +165,8 @@ function init() {
     controls.update();
     controls.enablePan = false;
     controls.enableDamping = true;
+    /* controls.minPolarAngle = controls.minAzimuthAngle;
+    controls.maxPolarAngle = controls.maxAzimuthAngle; */
 
     //
 
@@ -152,15 +193,47 @@ function animate() {
 
 }
 
+function submitInputs() {
+    input1 = clip(Number(input1Input.value), input1Min, input1Max);
+    input2 = clip(input2Input.value, input2Min, input2Max);
+    angle = input1;
+    sendValues();
+}
+
+function sendValues() {
+    input1Input.value = Math.round(input1 * 100) / 100;
+    input2Input.value = Math.round(input2 * 100) / 100;
+}
+
 function render() {
 
-    // TODO: Change camera target here
-
     controls.target.set(targetPos.x, targetPos.y, targetPos.z);
-
-    // TODO: change object positions, rotations, states, etc here
-
+    
+    rot += angle;
+    if (rot > 2 * Math.PI)
+    {
+        rot -= (2 * Math.PI);
+    }
+    if (cutout != undefined && btop != undefined && ttop != undefined)
+    {
+        spin(rot);
+    }
+    if (angle > 0)
+        angle -= 0.001;
+    else
+        angle = 0
+    /* document.getElementById("x").innerText = camera.position.x;
+    document.getElementById("y").innerText = camera.position.y;
+    document.getElementById("z").innerText = camera.position.z; */
     renderer.render(scene, camera);
+    
+}
+
+function spin (num)
+{
+    ttop.rotation.z = num;
+    cutout.rotation.z = num;
+    btop.rotation.z = (2 *Math.PI) - num;
 }
 
 function clip(input, limit1, limit2) {
